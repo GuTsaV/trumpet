9994f251426e1535e26e9b5d63473c5a03723c48
Gustav Ehrenborg <GuTsaV@users.noreply.github.com>
2022-01-20 16:32:37 +0100

    Add the code

---
+++ README.md
@@ -0,0 +1,43 @@
+# trumpet
+
+> **trumpet**
+>
+> *verb*
+>
+> proclaim widely or loudly.
+
+## Requirements
+
+* Withings scale, or other Withings hardware
+* Home Assistant
+* MQTT broker
+* Google Home or similar
+
+## Withings setup
+
+Follow this guide: 
+
+## Code Setup
+
+* `cp .env.sample .env`
+* Add variables to .env
+* `npm install`
+* `npm start`
+
+
+## Home Assistant
+
+In `automations.yml`, add the following, changing `topic`, `service` and `entity_id` according to your configuration. 
+
+```
+- alias: New weight
+  trigger:
+    platform: mqtt
+    topic: 'weight/current'
+    value_template: "{{ value_json }}"
+  action:
+    - service: tts.google_say
+      entity_id: media_player.dining_room_speaker
+      data:
+        message: "Your weight is {{ trigger.payload }} kilos"
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+const fs = require('fs').promises;
+const qs = require('qs');
+const axios = require('axios');
+const mqtt = require('async-mqtt');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { BASE_URL, PORT } = process.env;
+const { WITHINGS_CLIENT_ID, WITHINGS_CLIENT_SECRET } = process.env;
+const { MQTT_URL, MQTT_CHANNEL } = process.env;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT);
+
+const withingsAxios = axios.create({
+  baseURL: 'https://wbsapi.withings.net',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+});
+
+const getRefreshToken = async (userId) => fs.readFile(`./${userId}`, 'utf8');
+
+const persistRefreshToken = async (userId, refreshToken) => {
+  await fs.writeFile(`./${userId}`, refreshToken);
+};
+
+const refreshAccessToken = async (userId) => {
+  const currentRefreshToken = await getRefreshToken(28697229);
+
+  const data = {
+    action: 'requesttoken',
+    grant_type: 'refresh_token',
+    client_id: WITHINGS_CLIENT_ID,
+    client_secret: WITHINGS_CLIENT_SECRET,
+    refresh_token: currentRefreshToken,
+  };
+
+  const body = qs.stringify(data);
+
+  const response = await withingsAxios.post('/v2/oauth2', body);
+
+  const newRefreshAccessToken = response.data.body.refresh_token;
+  await persistRefreshToken(userId, newRefreshAccessToken);
+
+  return response.data.body.access_token;
+};
+
+const getMeasures = async (accessToken, startDate, endDate) => {
+  const data = {
+    action: 'getmeas',
+    meastype: 1,
+    category: 1,
+    startdate: startDate,
+    enddate: endDate,
+  };
+
+  const body = qs.stringify(data);
+
+  const response = await withingsAxios.post(
+    '/measure',
+    body,
+    { headers: { Authorization: `Bearer ${accessToken}` } },
+  );
+
+  return response.data.body.measuregrps;
+};
+
+const getLatestWeightFromMeasures = (measure) => measure[0].measures[0].value;
+
+const grams2Kilo = (grams) => grams / 1000;
+
+const broadcast = async (value) => {
+  const client = await mqtt.connectAsync(MQTT_URL);
+  await client.publish(MQTT_CHANNEL, value.toFixed(1));
+  await client.end();
+};
+
+app.post('/withingsWebhook', async (request, response) => {
+  response.send('ok');
+
+  const userId = request.body.userid;
+  const startDate = request.body.startdate;
+  const endDate = request.body.enddate;
+
+  const accessToken = await refreshAccessToken(userId);
+  const measures = await getMeasures(accessToken, startDate, endDate);
+  const weightInGrams = getLatestWeightFromMeasures(measures);
+  const weightInKilos = grams2Kilo(weightInGrams);
+  await broadcast(weightInKilos);
+});
+
+app.get('/url', async (_, response) => {
+  response.send(
+    `<a href="https://account.withings.com/oauth2_user/authorize2?response_type=code&client_id=${WITHINGS_CLIENT_ID}&state=whatever&scope=user.info,user.metrics&redirect_uri=${BASE_URL}/withingsWebhook">Click here</a>`,
+  );
+});
